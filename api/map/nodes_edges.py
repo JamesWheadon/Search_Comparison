8ce1dca89482c03a22e2380720b49f16a0a49a8f
James Wheadon <james.wheadon@yahoo.com>
2021-05-07 22:31:03 +0100

    functions to connect nodes with edges

--- api/map/nodes_edges.py
+++ api/map/nodes_edges.py
@@ -1,18 +1,31 @@
+import math
+
 class Node:
     def __init__(self, x, y, name=None):
         self.x = x
         self.y = y
+        if name is None:
+            name = f"N({self.x},{self.y})"
         self.name = name
         self.edges = []
     
     def __str__(self):
-        return f"Node at ({self.x}, {self.y})"
+        return f"Node at ({self.x},{self.y})"
+    
+    def connect_to_node(self, target_node):
+        new_edge = Edge(self, target_node)
+        self.edges.append(new_edge)
+        target_node.edges.append(new_edge)
 
 
 class Edge:
     def __init__(self, start, end):
         self.start = start
         self.end = end
+        self.weight = self.edge_length()
     
     def __str__(self):
-        return f"Edge connecting {start.name} to {end.name}"
+        return f"Edge connecting {self.start.name} to {self.end.name}"
+    
+    def edge_length(self):
+        return math.sqrt((self.start.x - self.end.x) ** 2 + (self.start.y - self.end.y) ** 2)
