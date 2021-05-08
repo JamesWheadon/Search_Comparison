e1c770a6d70ceb77b4e02008e545e2d888d141a0
James Wheadon <james.wheadon@yahoo.com>
2021-05-08 18:37:05 +0100

    recursive function to print tree

--- api/tree/tree.py
+++ api/tree/tree.py
@@ -3,4 +3,11 @@ from tree_node import TreeNode
 class Tree:
     def __init__(self, root):
         self.root = root
-        self.nodes = [root]
+        self.nodes = []
+    
+    def render_tree(self):
+        print(self.root.value)
+        if self.root.children:
+            for child in self.root.children[:-1]:
+                child.render_node(False, "")
+            self.root.children[-1].render_node(True, "")
--- api/tree/tree_node.py
+++ api/tree/tree_node.py
@@ -1,13 +1,32 @@
 class TreeNode:
-    def __init__(self, value):
+    def __init__(self, value, parent=None):
         self.value = value
         self.children = []
+        self.depth = 0
+    
+    def __str__(self):
+        return self.value
     
     def add_child(self, child):
-        if type(self) == type(child):
-            self.children.append(child)
+        if type(self.value) == type(child):
+            child_node = TreeNode(child)
+            child_node.depth = self.depth + 1
+            self.children.append(child_node)
+            return child_node
         else:
             raise ChildError('Incompatible child type')
+    
+    def render_node(self, last_child, previous):
+        if last_child:
+            print(previous + u'\u2514\u2500' + f"{self.value}")
+            children_previous = previous + u'   '
+        else:
+            print(previous + u'\u251c\u2500' + f"{self.value}")
+            children_previous = previous + u'\u2502  '
+        if self.children:
+            for child in self.children[:-1]:
+                child.render_node(False, children_previous)
+            self.children[-1].render_node(True, children_previous)
 
 
 class ChildError(Exception):
