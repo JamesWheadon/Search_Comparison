806f0929bab6c4f67536c9c741aab69e6325bbbf
James Wheadon <james.wheadon@yahoo.com>
2021-05-08 13:00:26 +0100

    add child to tree, custom error for wrong type

--- api/tree/tree.py
+++ api/tree/tree.py
@@ -3,4 +3,4 @@ from tree_node import TreeNode
 class Tree:
     def __init__(self, root):
         self.root = root
-        self.nodes = []
+        self.nodes = [root]
--- api/tree/tree_node.py
+++ api/tree/tree_node.py
@@ -1,4 +1,15 @@
 class TreeNode:
     def __init__(self, value):
         self.value = value
-        self.children = children
+        self.children = []
+    
+    def add_child(self, child):
+        if type(self) == type(child):
+            self.children.append(child)
+        else:
+            raise ChildError('Incompatible child type')
+
+
+class ChildError(Exception):
+    def __init__(self, message):
+        self.message = message
