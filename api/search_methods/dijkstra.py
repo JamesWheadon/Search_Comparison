e6c99018130b6c52485ab134ef04ac1290b6b1a2
James Wheadon <james.wheadon@yahoo.com>
2021-05-10 12:49:27 +0100

    custom error handling for incorrect start or target locations

--- api/search_methods/dijkstra.py
+++ api/search_methods/dijkstra.py
@@ -7,3 +7,26 @@ class DijkstraSearch:
         self.map = graph
         self.start = start
         self.target = target
+    
+    def find_start(self):
+        start_node = self.map.find_node(self.start[0], self.start[1])
+        if start_node:
+            self.start = start_node
+        else:
+            raise PositionError(self.start)
+    
+    def find_target(self):
+        target_node = self.map.find_node(self.target[0], self.target[1])
+        if target_node:
+            self.start = target_node
+        else:
+            raise PositionError(self.target)
+    
+    def search(self):
+        self.find_start()
+        self.find_target()
+    
+
+graph = Graph([[1, 1], [1, 1]])
+search = DijkstraSearch(graph, (0, 0), (1, 2))
+search.search()
