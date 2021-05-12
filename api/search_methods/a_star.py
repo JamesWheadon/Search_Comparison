b7dae43686d4e4bc6c6b324cf2aae10cad937a1f
James Wheadon <james.wheadon@yahoo.com>
2021-05-12 16:01:49 +0100

    set up A* search class and A* vertex class

--- api/search_methods/a_star.py
+++ api/search_methods/a_star.py
@@ -19,7 +19,7 @@ class DijkstraSearch:
         if start_node:
             self.start = start_node
             self.expanded_nodes.append(start_node)
-            start = TreeNode(DijkstraVertex(start_node, 0))
+            start = TreeNode(AStarVertex(start_node, 0))
             self.new_node = start
             self.tree = Tree(start)
         else:
@@ -32,34 +32,22 @@ class DijkstraSearch:
         else:
             raise PositionError(self.target)
     
-    def expand_vertex(self):
-        for edge in self.new_node.value.graph_node.edges:
-            if edge.end not in self.expanded_nodes:
-                vertex = DijkstraVertex(edge.end, self.new_node.value.f + edge.weight)
-                search_node = self.new_node.add_child(vertex)
-                self.potential_nodes.append(search_node)
-        self.expanded_nodes.append(self.new_node.value.graph_node)
-    
-    def new_vertex(self):
-        min_f_node = min(self.potential_nodes, key=attrgetter('value.f'))
-        self.new_node = min_f_node
-        self.potential_nodes = [node for node in self.potential_nodes if node.value.graph_node != self.new_node.value.graph_node]
-        if self.new_node.value.graph_node == self.target:
-            self.completed = True
-    
     def search(self):
         self.find_start()
         self.find_target()
-        while not self.completed:
-            self.expand_vertex()
-            self.new_vertex()
-        self.path = self.new_node.get_path()
         
 
-class DijkstraVertex:
-    def __init__(self, graph_node, distance):
+class AStarVertex:
+
+    def __init__(self, graph_node, distance, target, heuristic):
+        self.heuristic_methods = {'Manhattan': self.manhattan}
         self.graph_node = graph_node
-        self.f = distance
+        self.g = distance
+        self.h = self.heuristic_methods[heuristic](graph_node, target)
+        self.f = self.g + self.h
     
     def __str__(self):
         return f"Node: {self.graph_node.name}, distance: {self.f}"
+    
+    def manhattan(self, start_node, target_node):
+        return abs(start_node.x - target_node.x) + abs(start_node.y - target_node.y)
