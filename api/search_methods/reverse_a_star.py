40cecffcc07148af2a6d59cc7d21c23062fff174
James Wheadon <james.wheadon@yahoo.com>
2021-05-16 21:42:17 +0100

    Reverse A* class and db functions

--- api/search_methods/reverse_a_star.py
+++ api/search_methods/reverse_a_star.py
@@ -1,9 +1,17 @@
 import sqlite3
 
-connection = sqlite3.connect('reverse.db')
-
-with open('schema.sql') as f:
-    connection.executescript(f.read())
-
-connection.commit()
-connection.close()
+class ReverseAStar:
+    def __init__(self, graph, target):
+        self.map = graph
+        self.target = target
+    
+    def initialise_db(self):
+        connection = sqlite3.connect('reverse.db')
+        with open('./api/search_methods/schema.sql') as schema:
+            connection.executescript(schema.read())
+        connection.commit()
+        connection.close()
+    
+    def connecto_to_db(self):
+        db = sqlite3.connect('reverse.db')
+        return db
